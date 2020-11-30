4537dd70e66d5ac5d6c0381209950c3b0a38efa8
Alex Chiurtu <alex.chiurtu@gmail.com>
2020-11-30 10:35:58 +0200

    Initialize Model project

---
+++ TextClusterizer/TextClusterizer.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextClusterizer.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
